1e5d25a432c8660dadf38c98490e26106515d006
ArturoGit <arturosanma@gmail.com>
2026-07-18 08:35:26 +0200

    add profiles

--- src/password_generator/profiles.py
+++ src/password_generator/profiles.py
@@ -14,3 +14,25 @@ class PasswordProfile:
     use_uppercase: bool = True
     use_numbers: bool = True
     use_symbols: bool = True
+
+
+
+PROFILES: dict[str, PasswordProfile] = {
+    "web": PasswordProfile(
+        length=16,
+    ),
+    "wifi": PasswordProfile(
+        length=24,
+        use_symbols=False,
+    ),
+    "pin": PasswordProfile(
+        length=6,
+        use_lowecase=False,
+        use_uppercase=False,
+        use_numbers=True,
+        use_symbols=False,
+    ),
+    "secure": PasswordProfile(
+        length=32,
+    ),
+}
